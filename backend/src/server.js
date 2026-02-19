b96461328aefd7bbf4cac797fa314b7c8d08cabd
jaSmlx <lyshkov.mihail@gmail.com>
2026-02-20 02:56:17 +0300

    Изменения

--- backend/src/server.js
+++ backend/src/server.js
@@ -57,7 +57,7 @@ app.post('/api/orders', async (req, res) => {
     }
 });
 
-app.get('/*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
